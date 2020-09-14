adaabed660852087f2f504624053019db0f2d02c
rncn <doraidamon1rwfo@outlook.jp>
2020-09-14 23:00:42 +0900

    bugfix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Http\Controllers\TweetScheduleController;
-use App\Http\Controllers\AnalyzeController;
+use App\Http\Controllers\AnalyzerController;
 
 class Kernel extends ConsoleKernel
 {
