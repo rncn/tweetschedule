ef03615fabe1b1545fc40a831376e9710c6fe9cf
rncn <doraidamon1rwfo@outlook.jp>
2020-09-15 21:02:33 +0900

    y

--- app/Http/Controllers/AnalyzerController.php
+++ app/Http/Controllers/AnalyzerController.php
@@ -26,28 +26,28 @@ class AnalyzerController extends Controller
     private static function follower($user) {
         $twitter = TweetController::makeTwitter($user->id);
         //get users
-        $twitter->get('users/show', [
+        $twd =json_decode($twitter->get('users/show', [
             'screen_name' => $user->name
-        ]);
+        ]));
         
-        return $twitter['followers_count'];
+        return $twd['followers_count'];
     }
     private static function following($user) {
 
         $twitter = TweetController::makeTwitter($user->id);
         //get users
-        $twitter->get('users/show', [
+        $twd = json_decode($twitter->get('users/show', [
             'screen_name' => $user->name
-        ]);
-        return $twitter['freinds_count'];
+        ]));
+        return $twd['freinds_count'];
     }
     private static function listed($user) {
         
         $twitter = TweetController::makeTwitter($user->id);
         //get users
-        $twitter->get('users/show', [
+        $twd =json_decode($twitter->get('users/show', [
             'screen_name' => $user->name
-        ]);
-        return $twitter['listed_count'];
+        ]));
+        return $twd['listed_count'];
     }
 }
