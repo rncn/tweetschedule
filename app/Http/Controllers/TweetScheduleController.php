08aa398fab9ad611c2bdb2ef9fa5707eb74eda02
rncn <doraidamon1rwfo@outlook.jp>
2020-09-17 05:15:50 +0900

    fixed

--- app/Http/Controllers/TweetScheduleController.php
+++ app/Http/Controllers/TweetScheduleController.php
@@ -41,7 +41,7 @@ class TweetScheduleController extends Controller
                 "status" => $tweet->content
             ]));
             $tweet->delete();
-            Log::debug('予約投稿ed' . get_object_vars($res));
+            Log::debug('予約投稿ed');
         }
         return;
     }
